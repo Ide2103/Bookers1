0199533b52f5c6f724dc57e279b9b4323780852f
Ide2103 <107347502+Ide2103@users.noreply.github.com>
2022-07-16 07:51:07 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'books' => 'books#index'
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  get '/top' => 'homes#top'
+  get  root :to => 'homes#top'
+  resources :books
   post 'books' => 'books#create'
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
